beea23ab682d56b78d34597ef9d2a644722042ca
KindaSnoowy <gabrielcecote1@gmail.com>
2022-11-16 21:57:06 -0300

    nao ta funcionando (testando ainda po)

--- python/server.py
+++ python/server.py
@@ -2,10 +2,17 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 6060))
-s.listen(5)
+s.listen()
+print("Aguardando conexão!")
+
+
+clientSocket, address = s.accept()
+print(f"Conexão estabelecida no endereço: {address}")
 
 while True:
-    clientSocket, address = s.accept()
-    print(f"Conexão estabelecida no endereço: {address}")
-    clientSocket.send(bytes("Welcome to the Server!!!", "utf-8"))
-    clientSocket.close()
+    data = clientSocket.recv(2064)
+    if not data:
+        print("fechando conexão")
+        clientSocket.close()
+        break
+    clientSocket.sendall(data)
